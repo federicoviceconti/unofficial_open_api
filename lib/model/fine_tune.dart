bc8c11b9c28aff582962b5ae61a952e2a46a2dc0
Federico Viceconti <viceconti.federico@gmail.com>
2023-01-14 16:33:47 +0100

    fix epochs

--- lib/model/fine_tune.dart
+++ lib/model/fine_tune.dart
@@ -11,16 +11,16 @@ class FineTune with _$FineTune {
     final String? id,
     final String? object,
     final String? model,
-    @JsonKey(name: "created_at") final int? createdAt,
+    @JsonKey(name: 'created_at') final int? createdAt,
     final List<FineTuneEvent>? events,
-    @JsonKey(name: "fine_tuned_model") final String? fineTunedModel,
+    @JsonKey(name: 'fine_tuned_model') final String? fineTunedModel,
     final HyperParameters? hyperparams,
-    @JsonKey(name: "organization_id") final String? organizationId,
-    @JsonKey(name: "result_files") final List<File>? resultFiles,
+    @JsonKey(name: 'organization_id') final String? organizationId,
+    @JsonKey(name: 'result_files') final List<File>? resultFiles,
     final String? status,
-    @JsonKey(name: "training_files") final List<File>? trainingFiles,
-    @JsonKey(name: "updated_at") final int? updatedAt,
-    @JsonKey(name: "validation_files") final List<File>? validationFiles,
+    @JsonKey(name: 'training_files') final List<File>? trainingFiles,
+    @JsonKey(name: 'updated_at') final int? updatedAt,
+    @JsonKey(name: 'validation_files') final List<File>? validationFiles,
   }) = _FineTune;
 
   factory FineTune.fromJson(Map<String, dynamic> json) =>
@@ -31,7 +31,7 @@ class FineTune with _$FineTune {
 class FineTuneEvent with _$FineTuneEvent {
   const factory FineTuneEvent({
     final String? object,
-    @JsonKey(name: "created_at") final int? createdAt,
+    @JsonKey(name: 'created_at') final int? createdAt,
     final String? level,
     final String? message,
   }) = _FineTuneEvent;
@@ -43,11 +43,11 @@ class FineTuneEvent with _$FineTuneEvent {
 @freezed
 class HyperParameters with _$HyperParameters {
   const factory HyperParameters({
-    @JsonKey(name: "batch_size") final String? batchSize,
-    @JsonKey(name: "learning_rate_multiplier")
+    @JsonKey(name: 'batch_size') final String? batchSize,
+    @JsonKey(name: 'learning_rate_multiplier')
         final double? learningRateMultiplier,
-    final int? nEpochs,
-    @JsonKey(name: "prompt_loss_weight") final double? promptLossWeight,
+    @JsonKey(name: 'n_epochs') final int? nEpochs,
+    @JsonKey(name: 'prompt_loss_weight') final double? promptLossWeight,
   }) = _HyperParameters;
 
   factory HyperParameters.fromJson(Map<String, dynamic> json) =>
